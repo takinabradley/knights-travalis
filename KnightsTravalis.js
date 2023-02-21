0c2011ef137c5d2e2388663d47b3f9ab382208bf
takinabradley <bradfinney98@gmail.com>
2023-02-21 03:04:34 -0500

    add code comments

--- KnightsTravalis.js
+++ KnightsTravalis.js
@@ -33,39 +33,48 @@ const isInPath = (coord, path) => {
 function findDestination(currentPosition, destination, count = 0, path = []) {
   // FixMe! I'm slow :(
   if(Array.isArray(currentPosition)) {
+    // if currentPosition is just an array of coords, turn it into a graph node
     currentPosition = GraphNode(currentPosition)
     path = [currentPosition.value]
   }
 
+  // if the graph node's value is equal to the destination, return this path
   if (coordsAreEqual(currentPosition.value, destination)) return {count: count, path, info: 'was equal to current position'}
+
+  // if count is greater than the maximum path length, return -1
   if(count > 6) return {count: -1}
 
+  // add all possible, valid next move to the children of our graphnode
   allPossibleMoves.forEach((move) => {
     const nextPosition = [currentPosition.value[0] + move[0], currentPosition.value[1] + move[1]];
     if (!isOutOfBounds(nextPosition)) currentPosition.children.push(GraphNode(nextPosition));
   });
 
-  console.log([...currentPosition.children])
+  // if our destination is in the children of our node, return that node
   const foundInChildren = currentPosition.children.find((coord) => coordsAreEqual(coord.value, destination))
   if(foundInChildren) return {count: count + 1, path: [...path, destination], info: 'found in children'}
 
+  // create an array of paths with a 'count' score
   let countArray = [];
   currentPosition.children.forEach((move) => {
     if(isInPath(move.value, path)) return // maybe this makes it faster?
     countArray.push(findDestination({...move}, destination, count + 1, [...path, move.value]));
   });
 
+  // sort paths by lowest score
   countArray.sort( (a, b) => {
     if(a.count > b.count) return 1
     if(a.count < b.count) return -1
     return 0
   });
 
+  // remove all negative scores
   countArray = countArray.reduce( (array, item) => {
     if(item.count < 0) return array
     return [...array, item]
   }, [])
-  console.log('countarray', countArray)
-  if(countArray.length === 0) return {count: -1}
+
+  // return nothing or path
+  if(countArray.length === 0) return 
   return countArray[0];
 }
